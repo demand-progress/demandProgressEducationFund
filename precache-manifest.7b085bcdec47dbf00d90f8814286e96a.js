364bb460aec7783f86d90102dd49b48027d18ad4
alejandro <alejo4373@gmail.com>
2019-05-22 17:49:41 -0400

    compile mobile responsive changes

---
+++ precache-manifest.7b085bcdec47dbf00d90f8814286e96a.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "8ae1da3b41e3e3457e9fb774a306fca3",
+    "url": "/index.html"
+  },
+  {
+    "revision": "1f2d3467a42c5eeeb827",
+    "url": "/static/css/main.aedeb5b1.chunk.css"
+  },
+  {
+    "revision": "6de01a32f93a7b95d413",
+    "url": "/static/js/2.38fbdceb.chunk.js"
+  },
+  {
+    "revision": "1f2d3467a42c5eeeb827",
+    "url": "/static/js/main.1f78128d.chunk.js"
+  },
+  {
+    "revision": "42ac5946195a7306e2a5",
+    "url": "/static/js/runtime~main.a8a9905a.js"
+  }
+]);
